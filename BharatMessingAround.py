303f251a50aef34225bd19e8aa584aeffbe6c5de
Bharat Sreekrishnavilas <bbskkd@umsystem.edu>
2021-10-20 17:20:06 -0500

    Still trying to build the board

--- BharatMessingAround.py
+++ BharatMessingAround.py
@@ -1,28 +1,33 @@
-# Ignore this file. It has nothing to do with the project
-# It's just an environment for Bharat to explore PyGame
+import pygame, sys
+from pygame.locals import *
 
-import sys, pygame
 pygame.init()
 
-size = width, height = 600, 600
-speed = [2, 2]
-black = 0, 0, 0
+FPS = 240 # frames per second setting
+fpsClock = pygame.time.Clock()
 
-screen = pygame.display.set_mode(size)
+# set up the window
+WIDTH = 1000
+HEIGHT = 1000
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+pygame.display.set_caption('Battleship')
 
-ball = pygame.image.load("chicken.png")
-ballrect = ball.get_rect()
+OCEAN_COLOR = (0, 105, 148)
+dogImg = pygame.image.load('dog.png')
+dogx = WIDTH * 0.25
+dogy = HEIGHT * 0.25
 
-while 1:
+
+while True: # the main game loop
+    SCREEN.fill(OCEAN_COLOR)
+    mouseX = pygame.mouse.get_pos()[0]
+    mouseY = pygame.mouse.get_pos()[1]
+    SCREEN.blit(dogImg, (mouseX, mouseY))
+    
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+    pygame.display.update()
+    fpsClock.tick(FPS)
