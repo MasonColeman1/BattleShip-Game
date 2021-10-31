e22f34f73d97fd4c3ae07dbae123e77f79bb89f0
Zach <zab2nd@umsystem.edu>
2021-10-31 13:42:50 -0500

    Added ships and random ship placement.

---
+++ Ship.py
@@ -0,0 +1,47 @@
+
+
+class Ship:
+    ship_id = 0                 #Debug Var
+    size = 2                    #Ship Size
+    orientation = "Vertical"    #Ship Orientation
+    positions = []              #List of Ship Positions
+    destroyed = False           #Whether Ship is destroyed
+
+
+    def __init__(self, s_id, size, ori, posit, destro):  #Constructor
+        self.id = s_id
+        self.size = size
+        self.orientation = ori
+        self.positions = posit
+        self.destroyed = destro
+
+    def clear(self):    #Function used to clear a ship to default state
+        self.id = 0
+        self.size = 2
+        self.orientation = "Vertical"
+        self.positions = []
+        self.destroyed = False
+    
+    def getDestroy(self):   #Used to return whether a ship is destroyed
+        return self.destroyed
+
+    def checkDestroy(self, posit_list):  #Used to update whether ship is destroyed
+        result = all(elem in posit_list  for elem in self.positions)
+
+        if (result):
+            destroyed = True
+
+
+        #Called during ship placement to ensure ships are not placed over each other
+    def checkPositOverride(self, posit_list):
+        for p in posit_list:
+            for q in self.positions:
+                if(p==q):
+                    return True
+        return False
+
+    def printPosit(self):   #Called when board debug prints locations of ships
+        for p in range(len(self.positions)):
+            print(self.positions[p], end="")
+            print()
+            
